263634db287c7fb37af372ed5154102d1eef1a2c
Craig Harman <craig@harmonicnewmedia.com>
2019-07-24 05:40:35 +0000

    Apply fixes from StyleCI

--- src/InertiaModel.php
+++ src/InertiaModel.php
@@ -5,10 +5,12 @@ namespace harmonic\InertiaTable;
 use Illuminate\Database\Eloquent\Model;
 use harmonic\InertiaTable\Traits\InertiaTable;
 
-class InertiaModel extends Model {
+class InertiaModel extends Model
+{
     use InertiaTable;
 
-    public function resolveRouteBinding($value) {
+    public function resolveRouteBinding($value)
+    {
         return $this->where('id', $value)->withTrashed()->first() ?? abort(404);
     }
 }

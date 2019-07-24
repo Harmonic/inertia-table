878490c10c6ecd77578e14d56fe521afd5cb2453
Craig Harman <craig@harmonicnewmedia.com>
2019-07-24 13:29:25 +0800

    Fix issue with editing soft deleted model

--- src/InertiaModel.php
+++ src/InertiaModel.php
@@ -5,7 +5,10 @@ namespace harmonic\InertiaTable;
 use Illuminate\Database\Eloquent\Model;
 use harmonic\InertiaTable\Traits\InertiaTable;
 
-class InertiaModel extends Model
-{
+class InertiaModel extends Model {
     use InertiaTable;
+
+    public function resolveRouteBinding($value) {
+        return $this->where('id', $value)->withTrashed()->first() ?? abort(404);
+    }
 }

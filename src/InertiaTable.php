849a15995ca9eeacbc101d05b878a4f81289b209
atymic <atymicq@gmail.com>
2019-12-05 20:21:31 +1100

    fix: don't require model to extend the inertia table one

--- src/InertiaTable.php
+++ src/InertiaTable.php
@@ -2,6 +2,7 @@
 
 namespace harmonic\InertiaTable;
 
+use Illuminate\Database\Eloquent\Model;
 use Inertia\Inertia;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Schema;
@@ -12,12 +13,12 @@ class InertiaTable
     /**
      * Generates inertia view data for model.
      *
-     * @param InertiaModel $model The model to use to retrieve data
+     * @param Model $model The model to use to retrieve data
      * @param array $columns An array of column names to send to front end (null for all columns)'
      * @param array $filterable A subset of the $columns array containing names of columsn that can be filtered
      * @return void
      */
-    public function index(InertiaModel $model, array $columns = null, array $filterable = null)
+    public function index(Model $model, array $columns = null, array $filterable = null)
     {
         $modelName = class_basename($model);
 
